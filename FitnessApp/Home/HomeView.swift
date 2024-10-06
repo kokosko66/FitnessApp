43c0bf92a4437771131ae5a9dd88bd036adb2e95
Rosen Raykov <rg.raikov.eu@gmail.com>
2024-10-06 12:50:45 +0300

    Made the mock data published, so the code looks for a change in the code

--- FitnessApp/Home/HomeView.swift
+++ FitnessApp/Home/HomeView.swift
@@ -13,14 +13,14 @@ class HomeViewModel: ObservableObject {
     @State var active: Int = 50
     @State var stand: Int = 8
     
-    var mockActivities = [
+    @Published var mockActivities = [
         Activity(id: 0, title: "Todays Steps", subtitle: "Goal: 10,000", image: "figure.walk", tintColor: .green, amount: "6,121"),
         Activity(id: 1, title: "Calories", subtitle: "Goal: 600", image: "flame", tintColor: .red, amount: "250"),
         Activity(id: 2, title: "Stand", subtitle: "Goal: 8 hours", image: "figure.stand", tintColor: .blue, amount: "5 hours"),
         Activity(id: 3, title: "Stand", subtitle: "Goal: 8 hours", image: "figure.stand", tintColor: .blue, amount: "5 hours")
     ]
     
-    var mockWorkouts = [
+    @Published var mockWorkouts = [
         Workout(id: 0, title: "Running", image: "figure.run", tintColor: .green, duration: "23 mins", date: "Aug 19", calories: "341 kcal"),
         Workout(id: 1, title: "Boxing", image: "figure.boxing", tintColor: .red, duration: "2 hours", date: "Aug 10", calories: "1000 kcal"),
         Workout(id: 2, title: "Skiing", image: "figure.skiing.downhill", tintColor: .blue, duration: "4 hours", date: "Aug 10", calories: "550 kcal"),
